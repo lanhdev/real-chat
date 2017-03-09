ed4c7b608fe50f2026ef18c4e6605ac15e5b873b
Lanh Hoang <lanh.hoangcong@gmail.com>
2017-03-09 16:34:29 +0700

    Add flash messages helper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require semantic
+
+$(document).on('turbolinks:load', function(){
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+    .closest('.message')
+    .transition('fade')
+    ;
+  });
+})
