a76b63cab027a58a565bdc5b404b6bab13884616
Lanh Hoang <lanh.hoangcong@gmail.com>
2017-03-10 00:32:23 +0700

    Add basic sign in/sign out mechanism

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,14 @@ class HomeController < ApplicationController
     @room = Room.new
     @room.messages.build
   end
+
+  def signin
+    session[:username] = params[:username].presence
+    redirect_back fallback_location: root_path
+  end
+
+  def signout
+    session[:username] = nil
+    redirect_back fallback_location: root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
+  post 'sign_in' => 'home#signin'
+  delete 'sign_out' => 'home#signout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
