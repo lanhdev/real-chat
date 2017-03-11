111e5aea8c5d7165f6be5ff31ee9b1a6c381eed3
Lanh Hoang <lanh.hoangcong@gmail.com>
2017-03-12 01:00:57 +0700

    Implement auto reload messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,22 @@
 class MessagesController < ApplicationController
+  def index
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
   # POST rooms/:room_id/messages
   def create
     @room = Room.find(params[:room_id])
     @message = @room.messages.build message_params
     if @message.save
+      unless current_user
+        session[:username] = @message.username
+      end
       redirect_to @room
     else
       flash[:error] = "#{@message.errors.full_messages.to_sentence}"
